7df2c16346d64d3c0a773f275edeb5576f305d49
Arends-3 <g.c.arends-3@umcutrecht.nl>
2025-09-08 14:55:51 +0200

    Bug formula

--- ml_utils.py
+++ ml_utils.py
@@ -70,6 +70,7 @@ class Trainer:
                 model_out = self.model(inputs)
 
                 output, *res = self.output_calculator.output_from_model_out(model_out, input_dirs, **kwargs)
+                
                 loss = self.loss_fn(output, labels, *res)
 
                 loss.backward()
